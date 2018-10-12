a54de5509e305f75cfa5b328a095d7ceaf984d42
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-12 03:02:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,9 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["Lisp", "C"]}
+  programmer_hash
     
 end
 
