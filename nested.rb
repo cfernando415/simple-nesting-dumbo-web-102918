7af43af67e8fbfe4b9535a85b40e22cf635d36b0
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-12 02:59:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
      }
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
-  
+  programmer_hash[:yukihiro_matsumoto]
     
 end
 
@@ -107,6 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay]
      
 end
 
@@ -127,4 +128,5 @@ def adding_to_dennis
         }
      }
     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash[:dennis_ritchie][:languages]
 end
